a36d4f7cd92ff7c5c197ec3b8798c14d5a3b2986
Fernando dos Santos <nando4803@gmail.com>
2022-02-10 01:36:42 +0100

    style: add global styles and theme file

---
+++ src/styles/theme.ts
@@ -0,0 +1,37 @@
+export default {
+  colors: {
+    primary: '#0070f3',
+    secondary: '#ff4081',
+    tertiary: '#f5f5f5',
+    quaternary: '#f5f5f5',
+    quinary: '#f5f5f5',
+    senary: '#f5f5f5',
+    white: '#ffffff',
+    black: '#000000',
+    gray: '#9e9e9e',
+    lightGray: '#f5f5f5',
+    darkGray: '#616161',
+    lightBlue: '#00bcd4',
+    darkBlue: '#006064',
+    lightGreen: '#4caf50'
+  },
+  fonts: {
+    primary: 'Roboto'
+  },
+  sizes: {
+    small: '1rem',
+    medium: '2rem',
+    large: '3rem'
+  },
+  lines: {
+    small: '1.5rem',
+    medium: '2.5rem',
+    large: '3.5rem'
+  },
+  shadows: {
+    small: '0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)'
+  },
+  transitions: {
+    small: '0.2s ease-in-out'
+  }
+}
